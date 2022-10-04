89132705a71e598e84eb05048d6310fe1fb45d45
Vikas Mali <vikasmali4303@gmail.com>
2022-10-04 06:25:56 +0530

    changes

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function  Header (){
+    return(
+        <div>
+            <header>
+                <div class="headerWrapper">
+                    Logo
+                </div>
+            </header>
+        </div>
+       
+    );
+}
+export default Header;
